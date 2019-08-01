de866bc1b275b686229c1dda9efa02805ae6c37b
Parramon <dparramon@AffinionDS.Com>
2019-08-01 17:21:44 -0500

    Working selector click

--- src/index.js
+++ src/index.js
@@ -11,12 +11,34 @@ const url = `${protocol}://${domain}/${subdomain}`;
   const browser = await puppeteer.launch({
         headless: false,
         executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
-        ignoreDefaultArgs: ['--disable-extensions']
+        ignoreDefaultArgs: ['--disable-extensions'],
+        headless: false,
+        args: [
+            '--start-maximized'
+        ]
   });
   const page = await browser.newPage();
+  await page.setViewport({
+    width: 1920,
+    height: 1080
+  });
+  await page.goto(url, {waitUntil: 'domcontentloaded',timeout: 100000});
 
-  await page.goto(url);
-  await page.screenshot({path: `./screenshots/${domain}-${page.title()}.png`});
+  let pageTitle = await page.title();
+  console.log(`Page loaded: ${pageTitle}`);
 
-  await browser.close();
+  await page.screenshot({path: `./screenshots/${domain}-${pageTitle}.png`});
+  console.log(`Screenshot created in: ./screenshots/${domain}-${pageTitle}.png`);
+  const metrics = await page.metrics();
+  console.log(metrics);
+
+  const input = await page.waitForSelector('body > div > div:nth-child(6) > div:nth-child(2) > div > button > span.filter-option');
+  await input.click('body > div > div:nth-child(6) > div:nth-child(2) > div > button > span.filter-option');
+  
+  await input.type('t3');
+  await page.keyboard.press('Enter');
+
+
+
+  // await browser.close();
 })();
