47e1c0abbc06be0a02024b435f4e0996fc9a66ac
Parramon <dparramon@AffinionDS.Com>
2019-08-01 18:01:47 -0500

    working with conexions sso

--- src/index.js
+++ src/index.js
@@ -6,8 +6,25 @@ const domain = 'cltsenvironments.loyaltytravelservices.com';
 const subdomain = 'SSOSimulator';
 const protocol = 'http';
 const url = `${protocol}://${domain}/${subdomain}`;
+const ssoSelection = {
+  environment: 'QA',
+  client: 'US Bank',
+  program: '1289',
+  theme: 'usbank',
+  languange: 'English (United States)',
+  points: '100000',
+  user: '999999B'
+}; 
 
 (async () => {
+  const select = async (context, selection, val) => {
+    var selectorValue = [selection, val];
+    await context.waitForSelector( selectorValue[0] + ' > div:nth-child(2) > div > div > ul', { timeout: 100000 }).then(() => console.log('got it'));
+    await context.click(`${selectorValue[0]} > div:nth-child(2) > div > button > span.filter-option`);
+    await context.type(`${selectorValue[0]} > div:nth-child(2) > div > div > div > input`, val);
+    await context.keyboard.press('Enter');
+    console.log('\x1b[36m', 'Element triggered: ', selectorValue.val,'\x1b[0m');
+  };
   const browser = await puppeteer.launch({
         headless: false,
         executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
@@ -32,13 +49,14 @@ const url = `${protocol}://${domain}/${subdomain}`;
   const metrics = await page.metrics();
   console.log(metrics);
 
-  const input = await page.waitForSelector('body > div > div:nth-child(6) > div:nth-child(2) > div > button > span.filter-option');
-  await input.click('body > div > div:nth-child(6) > div:nth-child(2) > div > button > span.filter-option');
-  
-  await input.type('t3');
-  await page.keyboard.press('Enter');
-
-
+  // const input = await page.waitForSelector('body > div > div:nth-child(6) > div:nth-child(2) > div > button > span.filter-option');
+  // await input.click('body > div > div:nth-child(6) > div:nth-child(2) > div > button > span.filter-option');
+  // await input.type('t3');
+  // await page.keyboard.press('Enter');
+  await select(page, 'body > div > div:nth-child(6)', ssoSelection.environment);
+  await select(page, 'body > div > div:nth-child(7)', ssoSelection.client);
+  await select(page, 'body > div > div:nth-child(8)', ssoSelection.program);
+  await select(page, 'body > div > div:nth-child(9)', ssoSelection.theme);
 
   // await browser.close();
 })();
